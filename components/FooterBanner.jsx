0c4727e67f80a42bef58adad71472a25a45fe8f2
elidvenega <venegaelid@gmail.com>
2022-06-21 19:49:34 -0700

    first commit

---
+++ components/FooterBanner.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Link from 'next/link';
+
+import { urlFor } from '../lib/client';
+
+const FooterBanner = ({ footerBanner }) => {
+  return (
+    <div className="footer-banner-container">
+     <div className="banner-desc">
+       <div className="left">
+
+       </div>
+       <div>
+        
+       </div>
+     </div>
+    </div>
+  )
+}
+
+export default FooterBanner;
