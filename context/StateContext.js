766d0bb1fe28088c8af11b70f16b8f9536010730
elidvenega <venegaelid@gmail.com>
2022-06-25 09:35:27 -0700

    Made cart work

--- context/StateContext.js
+++ context/StateContext.js
@@ -42,19 +42,20 @@ export const StateContext = ({ children }) => {
   const toggleCartItemQuantity = (id, value) => {
     foundProduct = cartItems.find((item) => item._id === id);
     index = cartItems.findIndex((product) => product._id === id);
+    const newCartItems = cartItems.filter((item) => item._id !== id)
 
     if (value === "inc") {
       setCartItems([
-        ...cartItems,
-        { ...FoundProduct, quantity: FoundProduct.quantity + 1 },
+        ...newCartItems,
+        { ...foundProduct, quantity: foundProduct.quantity + 1 },
       ]);
       setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price);
       setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1);
     } else if (value === "dec") {
       if (foundProduct.quantity > 1) {
         setCartItems([
-          ...cartItems,
-          { ...FoundProduct, quantity: FoundProduct.quantity - 1 },
+          ...newCartItems,
+          { ...foundProduct, quantity: foundProduct.quantity - 1 },
         ]);
         setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price);
         setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1);
